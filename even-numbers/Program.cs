0af98405042baf2542be4e155f745fdb454667d1
landa03 <60487799+landa03@users.noreply.github.com>
2020-02-15 20:17:07 -0700

    Add files via upload

---
+++ even-numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace even_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int num =0; num <= 10; num ++)
+                {
+                    if (num % 2 == 0)
+                    {
+                            Console.WriteLine(num);
+                    }
+                }
+        }
+    }
+}
